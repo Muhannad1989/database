e6dd6ba0f72f01b25e3257f6b2c74d995b435005
unknown <muhannadjudah@gmail.com>
2019-06-13 23:11:03 +0200

    change trigger name

--- Week2/myTrigger.sql
+++ Week2/myTrigger.sql
@@ -1,10 +1,11 @@
 delimiter $$
- CREATE TRIGGER my_trigger55
+DROP TRIGGER IF EXISTS my_trigger;
+ CREATE TRIGGER my_trigger
     BEFORE INSERT
         ON countrylanguage
             FOR EACH ROW
             BEGIN
-                DECLARE message VARCHAR(100);
+                DECLARE message VARCHAR(255);
                 DECLARE var1 VARCHAR(255) ;
                 SET var1= ( select count(countrylanguage.Language) as languages from country join countrylanguage on country.code = countrylanguage.CountryCode where countryCode = new.countryCode);
                 IF var1 >=10 
